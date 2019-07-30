9d80d7a217430d718aa7a86fb3963f5ab83891fd
global <liufeng2311@163.com>
2019-07-30 16:33:22 +0800

    初始化

---
+++ a-spring-cloud-practice/spring-eureka-config-server/src/main/java/com/beiming/SpringEurekaConfigServerApplication.java
@@ -0,0 +1,20 @@
+package com.beiming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@EnableEurekaClient  //不需要注册到服务中心
+@EnableConfigServer
+@RestController
+public class SpringEurekaConfigServerApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(SpringEurekaConfigServerApplication.class, args);
+	}
+}
