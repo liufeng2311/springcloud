d581b398974ea1aa9c25aa17d087741343acc282
global <liufeng2311@163.com>
2019-07-31 13:29:53 +0800

    gateway代码配置的路由不生效，yml里的路由生效，暂未解决

--- a-spring-cloud-practice/spring-eureka-gateway/src/main/java/com/beiming/SpringEurekaGatewayApplication.java
+++ a-spring-cloud-practice/spring-eureka-gateway/src/main/java/com/beiming/SpringEurekaGatewayApplication.java
@@ -3,6 +3,10 @@ package com.beiming;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Mono;
 
 /**
  *gateway不能引用web依赖，否则会导致路由转发失败 
@@ -16,4 +20,6 @@ public class SpringEurekaGatewayApplication {
 		
 		SpringApplication.run(SpringEurekaGatewayApplication.class, args);
 	}
+	
+	
 }
--- a-spring-cloud-practice/spring-eureka-gateway/src/main/java/com/beiming/config/GatewayConfig.java
+++ a-spring-cloud-practice/spring-eureka-gateway/src/main/java/com/beiming/config/GatewayConfig.java
@@ -3,28 +3,41 @@ package com.beiming.config;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Component
+import reactor.core.publisher.Mono;
+
+/**
+ *代码配置路由
+ *此处不起作用，未解决 
+ *
+ */
+@Configuration
 @RestController
 public class GatewayConfig {
 
 	//定义自己的路由规则
 	@Bean
 	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-	        return builder.routes()
-	            .route(p -> p
-	                .path("/client/sayHello")
-	                .filters(f -> f.addRequestHeader("Hello", "World"))
-	                .uri("localhost:9003"))
-	            .build();
+		return builder.routes()
+				.route("client",p -> p
+						.path("/client/**")
+						.filters(f -> f.addRequestHeader("Hello", "World"))
+						.uri("localhost:9003"))  //该属性只需要规定域名或者ip+port，
+				.route("ribbon",p -> p
+						.path("/ribbon/**")
+						.filters(f -> f
+								.hystrix(config -> config
+										.setName("hystrix")
+										.setFallbackUri("forward:/fallback")))
+						.uri("localhost:9003"))
+				.build();
 	}
-	
-/*	@Bean
-	public ServerCodecConfigurer serverCodecConfigurer() {
-		DefaultServerCodecConfigurer config = new DefaultServerCodecConfigurer();
-		return config;
-		
-	}*/
+
+	@RequestMapping("/fallback")
+	public Mono<String> fallback() {
+		return Mono.just("fallback");
+	}
 }
